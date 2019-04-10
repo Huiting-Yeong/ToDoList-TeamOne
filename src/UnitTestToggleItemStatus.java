2121ebb09ed0a766303d281a5c5a34d59115957a
issuser <issuser@ISS-CR-31221.stf.nus.edu.sg>
2019-04-10 14:17:36 +0800

    Add new test cases

---
+++ src/UnitTestToggleItemStatus.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTestToggleItemStatus {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+
+
+	//toggleItemStatus(Item it)
+
+}
