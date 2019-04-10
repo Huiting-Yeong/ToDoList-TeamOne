9098fd82efbdfc49b9b6918f557becad8336f86f
issuser <issuser@ISS-CR31202.stf.nus.edu.sg>
2019-04-10 15:17:35 +0800

    HT Commit: Update path for test

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,30 +2,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
+	public static boolean pauseMenu = false;
+	
 	public static void main(String[] args){
-		boolean exit = false;
-		do {
-			showMenu();
-			int input = readInput();
-			if (input==9) exit=true;
-		} while (!exit);
+		showMenu();
 	}
 	
 	public static void showMenu(){
-		System.out.println("Welcome to To Do List Application");
-		showCurrentList();
-		System.out.println("Menu: ");
-		System.out.println("1. List all to do list");
-		System.out.println("2. Add new item");
-		System.out.println("3. Delete an item");
-		System.out.println("4. Mark an item as complete");
-		System.out.println("9. Quit");
-		System.out.print("Enter your choice: ");
+		boolean exit = false;
+		do {
+			System.out.println("Welcome to To Do List Application");
+			showCurrentList();
+			System.out.println("Menu: ");
+			System.out.println("1. List all to do list");
+			System.out.println("2. Add new item");
+			System.out.println("3. Delete an item");
+			System.out.println("4. Mark an item as complete");
+			System.out.println("9. Quit");
+			System.out.print("Enter your choice: ");
+			
+			readInput();
+			if (pauseMenu) exit = true;
+		} while (!exit);
 	}
 	
 	public static int readInput(){
 		Scanner scanner = new Scanner(System.in);
 		int input = scanner.nextInt();	
+		pauseMenu = true;
 		
 		switch(input){
 		case 1:
@@ -41,12 +45,12 @@ public class MainClass {
 			break;
 			
 		case 9:
-
+			
 			break;
 		default:
+			pauseMenu = false;
 			System.out.println("Invalid choice");
 		}
-		
 		
 		return input;
 	}
