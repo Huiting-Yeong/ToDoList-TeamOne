37afadda3d5e509bdd0bb6c1f2444e0b34f36df4
issuser <issuser@ISS-CR-31221.stf.nus.edu.sg>
2019-04-10 15:35:40 +0800

    Update toggle test case

--- test/TestCaseToggleItemStatus.java
+++ test/TestCaseToggleItemStatus.java
@@ -3,10 +3,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestCaseToggleItemStatus {
-
+	private ToDoList testList = new ToDoList();
+	private Item item = new Item("Buy fruits");
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testToggleItemStatus()
+	{
+		//// Assume list has items
+		testList.addItem(item);
+		
+//		//// toggle an item
+//		Boolean isToggled = testList.toggleItemStatus(item);
+//		assertEquals(true, isToggled);
 	}
-
 }
