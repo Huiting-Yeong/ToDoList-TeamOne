4571512edb2f2c57337950b3b956103c9ec3c947
issuser <issuser@ISS-CR-31203.stf.nus.edu.sg>
2019-04-10 15:49:28 +0800

    Integrated main class to call our functions.

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
+	static ToDoList myList ;
+	
 	public static void main(String[] args){
 		boolean exit = false;
-		do {
+		myList = new ToDoList();
+		do {			
 			showMenu();
 			int input = readInput();
-			if (input==9) exit=true;
+			if (input == 9) exit=true;
 		} while (!exit);
 	}
 	
@@ -23,6 +26,29 @@ public class MainClass {
 		System.out.print("Enter your choice: ");
 	}
 	
+	public static String addUserItem(){
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		Item inputItem = new Item(input);
+		myList.addItem(inputItem);
+		return input;
+	}
+	
+	public static int removeUserItem(){
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		if (!myList.isEmpty() && (input-1 <= myList.size())) myList.removeItemByIndex(input-1);
+		return input;
+	}
+	
+	public static int toggleItem(){
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		Item toToggle = myList.itemList.get(input-1);
+		toToggle.toggleStatus();
+		return input;
+	}
+	
 	public static int readInput(){
 		Scanner scanner = new Scanner(System.in);
 		int input = scanner.nextInt();	
@@ -34,10 +60,21 @@ public class MainClass {
 			
 		case 2:
 			System.out.println("2");
+			System.out.println("Task to be added:");
+			addUserItem();
 			break;
 			
 		case 3:
 			System.out.println("3");
+			System.out.println("Enter the item number you want to remove. ");
+			removeUserItem();
+			System.out.print("Current size is now:" + myList.size());
+			break;
+			
+		case 4:
+			System.out.println("4");
+			System.out.println("Enter the item number that you want to toggle.");
+			toggleItem();
 			break;
 			
 		case 9:
@@ -52,14 +89,26 @@ public class MainClass {
 	}
 	
 	public static void showCurrentList(){
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("Fix bug no 1");
-		list.add("Fix bug no 2");
-		list.add("Fix bug no 3");
-		
-		for (String s : list){
-			System.out.println(s);
+		int count = 0;
+		for (Item s : myList.itemList){
+			count++;
+			if (s.done){
+				System.out.println(count + ".  "+ "X  " + s.desc);
+			}
+			else {
+				System.out.println(count + ".  "+ "O  " + s.desc);
+			}
+			
 		}
+		
+//		ArrayList<String> list = new ArrayList<String>();
+//		list.add("Fix bug no 1");
+//		list.add("Fix bug no 2");
+//		list.add("Fix bug no 3");
+//		
+//		for (String s : list){
+//			System.out.println(s);
+//		}
 		
 	}
 }
