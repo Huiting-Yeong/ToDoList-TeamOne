47a11b4371b61512deaca18f5f176c16577b8475
issuser <issuser@ISS-CR-31221.stf.nus.edu.sg>
2019-04-10 15:19:05 +0800

    Update remove test case

--- test/TestCaseRemoveItem.java
+++ test/TestCaseRemoveItem.java
@@ -3,24 +3,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestCaseRemoveItem {
-
+	private ToDoList testList = new ToDoList();
+	private Item item = new Item("Buy fruits");
+	
 	@Test
-	public void testAddNewItem()
+	public void testRemoveItem()
 	{
+		//// Assume list has items
+		testList.addItem(item);
 		
+		//// Remove an item
+		Boolean isremoved = testList.removeItem(item);
+		assertEquals(true, isremoved);
 	}
 	
 	@Test
-	public void testAddExistingItem()
+	public void testRemoveExistingItem()
 	{
-		
+		Boolean isremoved = testList.removeItem(item);
+		assertEquals(false, isremoved);
 	}
 	
 	@Test
-	public void testAddEmptyItem()
+	public void testRemoveEmptyItem()
 	{
-		
+		Boolean isremoved = testList.removeItem(new Item(""));
+		assertEquals(false, isremoved);
 	}
-	//removeItem(Item it)
-
 }
